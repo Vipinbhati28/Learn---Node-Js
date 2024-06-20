d31ce59d2a623bc658f50448d271b39d1ef84283
Vipinbhati28 <140482820+Vipinbhati28@users.noreply.github.com>
2024-06-20 13:17:17 +0000

    container config done

---
+++ .devcontainer/01_basicJavascript/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //"use strict" used to treat all JS code as newer version so that previous code doesn't affected 
+
+console.log("Vipin");
+
+let name = "Vipin";
+let age = 21;
+
+// number => 2 to power 53
+// bigint used when we took very very big value of int
+// boolean => true/false
+// null => it is a standalone value 
+// undefined => 
+// symbol => used for uniqueness    
+
+
+console.log(typeof age); // used to finding type of data
