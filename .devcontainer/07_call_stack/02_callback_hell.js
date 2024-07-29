c5f3049dd219af8631f1eb52b45fe062b13ee966
Vipinbhati28 <140482820+Vipinbhati28@users.noreply.github.com>
2024-07-29 17:20:04 +0000

    container config done

--- .devcontainer/07_call_stack/02_callback_hell.js
+++ .devcontainer/07_call_stack/02_callback_hell.js
@@ -1,60 +1,78 @@
-// for changing colors using in a given time interval
-// below is a callback nesting  =>  call back hell
-h1 = document.querySelector("h1");
+// // for changing colors using in a given time interval
+// // below is a callback nesting  =>  call back hell
+// h1 = document.querySelector("h1");
 
-function changeColor(color, delay, nextColorChange) {
-    setTimeout( () => {
-        h1.style.color = color;
-        if(nextColorChange) nextColorChange();
-    }, delay);
-}
+// function changeColor(color, delay, nextColorChange) {
+//     setTimeout( () => {
+//         h1.style.color = color;
+//         if(nextColorChange) nextColorChange();
+//     }, delay);
+// }
 
-changeColor("red", 1000, () => {
-    changeColor("orange", 1000, () => {
-        changeColor("green", 1000, () => {
-            changeColor("blue", 1000);
-        });
+// changeColor("red", 1000, () => {
+//     changeColor("orange", 1000, () => {
+//         changeColor("green", 1000, () => {
+//             changeColor("blue", 1000);
+//         });
+//     });
+// });
+
+// // this is call back hell because it is not a good way
+// // To preventing from call back hell there are Promises
+
+
+
+// function savetoDb(data, success, failure){
+//     let internetSpeed = Math.floor(Math.random() * 10) + 1;
+//     if(internetSpeed > 4) {
+//         success();
+//     } else {
+//         failure();
+//     }
+// }
+
+// savetoDb(
+//     "Chai Aur Code",
+//     () => {
+//         console.log("success : your data was saved");
+//         savetoDb(
+//             "Hello world",
+//             () => {
+//                 console.log("success2: data2 saved");
+//                 savetoDb(
+//                     "Vipin",
+//                     () => {
+//                         console.log("success3: data3 saved");
+//                     },
+//                     () => {
+//                         console.log("failure3: weak connection");
+//                     }
+//                 );
+//             },
+//             () => {
+//                 console.log("failure2 : weak connection");
+//             }
+//         );
+//     },
+//     () => {
+//         console.log("failure: weak connection. data not saved");
+//     }
+// );
+
+
+
+
+// here Below we use promises
+// Promises JS ki asynchronous nature wale code ke baare mein btata hai ki vo resolve hoga ya reject aur fir jo bhi final value aayegi.
+function savetoDb(data) {
+    return new Promise((resolve, reject) => {
+        let internetSpeed = Math.floor(Math.random() * 10) + 1;
+        if (internetSpeed > 4) {
+            resolve("success: data was saved");
+        } else {
+            reject("failure: weak connection");
+        }
     });
-});
-
-// this is call back hell because it is not a good way
-// To preventing from call back hell there are Promises
-
-
-
-function savetoDb(data, success, failure){
-    let internetSpeed = Math.floor(Math.random() * 10) + 1;
-    if(internetSpeed > 4) {
-        success();
-    } else {
-        failure();
-    }
 }
 
-savetoDb(
-    "Chai Aur Code",
-    () => {
-        console.log("success : your data was saved");
-        savetoDb(
-            "Hello world",
-            () => {
-                console.log("success2: data2 saved");
-                savetoDb(
-                    "Vipin",
-                    () => {
-                        console.log("success3: data3 saved");
-                    },
-                    () => {
-                        console.log("failure3: weak connection");
-                    }
-                );
-            },
-            () => {
-                console.log("failure2 : weak connection");
-            }
-        );
-    },
-    () => {
-        console.log("failure: weak connection. data not saved");
-    }
-);
+savetoDb("Chai Aur Code");
