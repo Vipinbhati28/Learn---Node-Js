e7dead565a4ff8b09a5c1e87eba0233522d378ea
Vipinbhati28 <140482820+Vipinbhati28@users.noreply.github.com>
2024-07-29 16:41:52 +0000

    container config done

--- .devcontainer/07_call_stack/02_callback_hell.js
+++ .devcontainer/07_call_stack/02_callback_hell.js
@@ -19,3 +19,42 @@ changeColor("red", 1000, () => {
 
 // this is call back hell because it is not a good way
 // To preventing from call back hell there are Promises
+
+
+
+function savetoDb(data, success, failure){
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+    if(internetSpeed > 4) {
+        success();
+    } else {
+        failure();
+    }
+}
+
+savetoDb(
+    "Chai Aur Code",
+    () => {
+        console.log("success : your data was saved");
+        savetoDb(
+            "Hello world",
+            () => {
+                console.log("success2: data2 saved");
+                savetoDb(
+                    "Vipin",
+                    () => {
+                        console.log("success3: data3 saved");
+                    },
+                    () => {
+                        console.log("failure3: weak connection");
+                    }
+                );
+            },
+            () => {
+                console.log("failure2 : weak connection");
+            }
+        );
+    },
+    () => {
+        console.log("failure: weak connection. data not saved");
+    }
+);
