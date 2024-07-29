eee40c8a80aa2090248d2b5fd7101e954bc62c13
Vipinbhati28 <140482820+Vipinbhati28@users.noreply.github.com>
2024-07-29 04:21:40 +0000

    container config done

---
+++ .devcontainer/07_call_stack/02_callback_hell.js
@@ -0,0 +1,21 @@
+// for changing colors using in a given time interval
+// below is a callback nesting  =>  call back hell
+h1 = document.querySelector("h1");
+
+function changeColor(color, delay, nextColorChange) {
+    setTimeout( () => {
+        h1.style.color = color;
+        if(nextColorChange) nextColorChange();
+    }, delay);
+}
+
+changeColor("red", 1000, () => {
+    changeColor("orange", 1000, () => {
+        changeColor("green", 1000, () => {
+            changeColor("blue", 1000);
+        });
+    });
+});
+
+// this is call back hell because it is not a good way
+// To preventing from call back hell there are Promises
